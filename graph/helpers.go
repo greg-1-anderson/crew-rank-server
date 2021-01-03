b07913168619db6f56fd1fccb8631e714915ad85
Greg Anderson <greg.1.anderson@greenknowe.org>
2021-01-03 15:30:16 -0800

    Create preliminary schema and return some mock data.

--- graph/helpers.go
+++ graph/helpers.go
@@ -1,3 +1,27 @@
 package graph
 
+import "twta/crew-rank-server/graph/model"
+
 //go:generate go run github.com/99designs/gqlgen
+
+func playerAggrageteStats(name string, imposter *model.ImposterStats, crewmate *model.CrewmateStats) *model.PlayerAggragateStats {
+	// Fill in / correct the total number of games
+	imposter.Games.Total = imposter.Games.Won + imposter.Games.Lost
+	crewmate.Games.Total = crewmate.Games.Won + crewmate.Games.Lost
+
+	// Calculate the overall game statistics
+	overall := model.AggragateStats{
+		Won:   imposter.Games.Won + crewmate.Games.Won,
+		Lost:  imposter.Games.Lost + crewmate.Games.Lost,
+		Total: imposter.Games.Total + crewmate.Games.Total,
+	}
+
+	return &model.PlayerAggragateStats{
+		Name: name,
+		Overall: &model.OverallStats{
+			Games: &overall,
+		},
+		Imposter: imposter,
+		Crewmate: crewmate,
+	}
+}
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,24 +5,38 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"twta/crew-rank-server/graph/generated"
 	"twta/crew-rank-server/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
+func (r *queryResolver) CrewRank(ctx context.Context) (*model.CrewRank, error) {
+	ladder := []*model.PlayerAggragateStats{}
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
+	ladder = append(
+		ladder,
+		playerAggrageteStats(
+			"The Dead Body",
+			&model.ImposterStats{
+				Games: &model.AggragateStats{
+					Won:  10,
+					Lost: 50,
+				},
+			},
+			&model.CrewmateStats{
+				Games: &model.AggragateStats{
+					Won:  40,
+					Lost: 10,
+				},
+			},
+		),
+	)
 
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+	return &model.CrewRank{
+		Ladder: ladder,
+	}, nil
+}
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
